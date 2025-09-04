21edc5113e534dac1f053bca2789a672a1e21505
leonardo <leonardo.maia07@gmail.com>
2025-09-04 11:32:17 -0400

    javascrip-dom

---
+++ javascript/functions.js
@@ -0,0 +1,18 @@
+function imprimirOla(){
+    console.log("Olá");
+}
+
+imprimirOla()
+
+function imprimirSoma(var1, var2){
+    console.log(var1 + var2) ;
+}
+
+imprimirSoma(1,1)
+
+function retornaASoma(var3, var4){
+    return (var3 + var4);
+}
+
+let soma = retornaASoma(2,3);
+console.log(soma)
---
+++ javascript/preenchendo-um-select/script.js
@@ -0,0 +1,33 @@
+const container = document.getElementById('app');
+
+// criar label
+
+const label = document.createElement('label');
+label.htmlFor = 'frutas-select';
+label.textContent = 'Escolha uma fruta: ';
+label.style.fontWeight = 'bold';
+label.classList.add('form-label');
+label.classList.add('m-1');
+
+// // criar select
+
+const select = document.createElement('select');
+select.id = 'frutas-select';
+select.className = 'form-select';
+
+
+const fruits = ['maçã','Banana', 'Laranja', 'Uva', 'Abacaxi'];
+
+
+fruits.forEach((fruit) => {
+    const option = document.createElement('option');
+    option.value = fruit.toLowerCase();
+    option.textContent = fruit;
+    select.appendChild(option);
+});
+
+container.appendChild(label);
+
+container.appendChild(select);
+
+
